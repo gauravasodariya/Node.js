de1abc7e9c17631472db53fe9a39b7fe30d94473
gauravasodariya <gauravasodariya44@gmail.com>
2025-01-19 18:05:22 +0530

    app.js

--- First Node Server/app.js
+++ First Node Server/app.js
@@ -1,1 +1,11 @@
+const http=require('http'); 
 
+function requestListener(req,res)
+{
+    console.log(req);
+}  
+const server=http.createServer(requestListener);
+server.listen(3000,() =>{
+    console.log(`Server running on address http://localhost:${3000}`);
+}
+);
